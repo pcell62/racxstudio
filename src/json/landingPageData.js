a8dff6288ce1f55a6b851d1fa4cc792ab36e4366
pcell62 <priyanshukumar10714@gmail.com>
2024-11-16 21:07:03 +0530

    75757

--- src/json/landingPageData.js
+++ src/json/landingPageData.js
@@ -17,6 +17,21 @@ import Favorite from "../assets/images/Advantages/Favorite.png";
 import JalMayo from "../assets/images/Portfolio/jal_mayo.jpg";
 import DarkSoy from "../assets/images/Portfolio/dark_soy.jpg";
 import BarMayo from "../assets/images/Portfolio/bar_mayo.jpg";
+import EggMayo from "../assets/images/Portfolio/egg_mayo.jpg";
+import ItaMayo from "../assets/images/Portfolio/ItaMayo.jpg";
+import GarMayo from "../assets/images/Portfolio/GarMayo.jpg";
+import TandMayo from "../assets/images/Portfolio/TandMayo.jpg";
+import PeriMayo from "../assets/images/Portfolio/PeriMayo.jpg";
+import Sandwhich from "../assets/images/Portfolio/Sandwich.jpg";
+import Classic from "../assets/images/Portfolio/Classic.jpg";
+import GreChil from "../assets/images/Portfolio/GreenChilli.jpg";
+import PizPas from "../assets/images/Portfolio/PizzPas.jpg";
+import MintMayo from "../assets/images/Portfolio/MintMayo.jpg";
+import Schez from "../assets/images/Portfolio/Schez.jpg";
+import Ginger from "../assets/images/Portfolio/Ginger.jpg";
+import Garlic from "../assets/images/Portfolio/Garlic.jpg";
+import RedCHill from "../assets/images/Portfolio/RedChill.jpg";
+import TomKet from "../assets/images/Portfolio/TomKet.jpg";
 
 export const Services = [
   {
@@ -44,13 +59,13 @@ export const Portfolios = [
     id: "sauce_001",
     title: "Red Chilly Sauce",
     type: "Sauces",
-    imageUrl: "RedChillySauce.jpg",
+    imageUrl: RedCHill,
   },
   {
     id: "sauce_002",
     title: "Green Chilly Sauce",
     type: "Sauces",
-    imageUrl: "GreenChillySauce.jpg",
+    imageUrl: GreChil,
   },
   {
     id: "sauce_003",
@@ -62,205 +77,73 @@ export const Portfolios = [
     id: "sauce_004",
     title: "Sezchwan Sauce",
     type: "Sauces",
-    imageUrl: "SezchwanSauce.jpg",
-  },
-  {
-    id: "sauce_005",
-    title: "8 to 8 Sauce - 200g",
-    type: "Sauces",
-    imageUrl: "8To8Sauce.jpg",
-  },
-  {
-    id: "sauce_006",
-    title: "8 to 9 - 200g",
-    type: "Sauces",
-    imageUrl: "8To9Sauce.jpg",
-  },
-  {
-    id: "sauce_007",
-    title: "Worsestershire Sauce",
-    type: "Sauces",
-    imageUrl: "WorsestershireSauce.jpg",
-  },
-  {
-    id: "sauce_008",
-    title: "Tomato Sauce",
-    type: "Sauces",
-    imageUrl: "TomatoSauce.jpg",
+    imageUrl: Schez,
   },
   {
     id: "sauce_009",
     title: "Tomato Ketchup",
     type: "Sauces",
-    imageUrl: "TomatoKetchup.jpg",
-  },
-  {
-    id: "sauce_010",
-    title: "Sweet Chilli Sauce",
-    type: "Sauces",
-    imageUrl: "SweetChilliSauce.jpg",
+    imageUrl: TomKet,
   },
   {
     id: "sauce_011",
     title: "Pizza Sauce",
     type: "Sauces",
-    imageUrl: "PizzaSauce.jpg",
-  },
-  {
-    id: "sauce_012",
-    title: "Tamarind Sauce",
-    type: "Sauces",
-    imageUrl: "TamarindSauce.jpg",
-  },
-  {
-    id: "sauce_013",
-    title: "Peri Peri Sauce",
-    type: "Sauces",
-    imageUrl: "PeriPeriSauce.jpg",
-  },
-  {
-    id: "sauce_014",
-    title: "Mustard Sauce",
-    type: "Sauces",
-    imageUrl: "MustardSauce.jpg",
-  },
-  {
-    id: "sauce_015",
-    title: "Red Hot Papper Sauce (Tabasco)",
-    type: "Sauces",
-    imageUrl: "RedHotPapperSauce.jpg",
-  },
-  {
-    id: "sauce_016",
-    title: "Green Hot Papper Sauce (Tabasco)",
-    type: "Sauces",
-    imageUrl: "GreenHotPapperSauce.jpg",
-  },
-  {
-    id: "chutney_001",
-    title: "Corriender Chutney",
-    type: "Chutneys and Paste",
-    imageUrl: "CorrienderChutney.jpg",
-  },
-  {
-    id: "chutney_002",
-    title: "Mint Chutney",
-    type: "Chutneys and Paste",
-    imageUrl: "MintChutney.jpg",
-  },
-  {
-    id: "chutney_003",
-    title: "Corriender Mint Chutney",
-    type: "Chutneys and Paste",
-    imageUrl: "CorrienderMintChutney.jpg",
-  },
-  {
-    id: "chutney_004",
-    title: "Green Chilly Chutney",
-    type: "Chutneys and Paste",
-    imageUrl: "GreenChillyChutney.jpg",
-  },
-  {
-    id: "chutney_005",
-    title: "Garlic Chilly Chutney",
-    type: "Chutneys and Paste",
-    imageUrl: "GarlicChillyChutney.jpg",
-  },
-  {
-    id: "chutney_006",
-    title: "Panipuri Chutney",
-    type: "Chutneys and Paste",
-    imageUrl: "PanipuriChutney.jpg",
-  },
-  {
-    id: "chutney_007",
-    title: "Samosa Chutney",
-    type: "Chutneys and Paste",
-    imageUrl: "SamosaChutney.jpg",
-  },
-  {
-    id: "chutney_008",
-    title: "Date & Tamarind Chutney",
-    type: "Chutneys and Paste",
-    imageUrl: "DateTamarindChutney.jpg",
-  },
-  {
-    id: "chutney_009",
-    title: "Sezchwan Chutney",
-    type: "Chutneys and Paste",
-    imageUrl: "SezchwanChutney.jpg",
+    imageUrl: PizPas,
   },
   {
     id: "paste_001",
     title: "Garlic Paste",
     type: "Chutneys and Paste",
-    imageUrl: "GarlicPaste.jpg",
+    imageUrl: Garlic,
   },
   {
     id: "paste_002",
     title: "Ginger Paste",
     type: "Chutneys and Paste",
-    imageUrl: "GingerPaste.jpg",
-  },
-  {
-    id: "paste_003",
-    title: "Garlic Ginger Paste Mixed",
-    type: "Chutneys and Paste",
-    imageUrl: "GarlicGingerPaste.jpg",
-  },
-  {
-    id: "paste_004",
-    title: "Mustard Paste",
-    type: "Chutneys and Paste",
-    imageUrl: "MustardPaste.jpg",
+    imageUrl: Ginger,
   },
   {
     id: "mayo_001",
     title: "Eggless Mayonnaise",
     type: "Mayonnaise",
-    imageUrl: "EgglessMayonnaise.jpg",
+    imageUrl: EggMayo,
   },
   {
     id: "mayo_002",
     title: "Tandoori Mayonnaise",
     type: "Mayonnaise",
-    imageUrl: "TandooriMayonnaise.jpg",
+    imageUrl: TandMayo,
   },
   {
     id: "mayo_003",
     title: "Peri Peri Mayonnaise",
     type: "Mayonnaise",
-    imageUrl: "PeriPeriMayonnaise.jpg",
+    imageUrl: PeriMayo,
   },
   {
     id: "mayo_004",
     title: "Garlic Mayonnaise",
     type: "Mayonnaise",
-    imageUrl: "GarlicMayonnaise.jpg",
+    imageUrl: GarMayo,
   },
   {
     id: "mayo_005",
     title: "Mint Mayonnaise",
     type: "Mayonnaise",
-    imageUrl: "MintMayonnaise.jpg",
+    imageUrl: MintMayo,
   },
   {
     id: "mayo_006",
     title: "Italian Herb Mayonnaise",
     type: "Mayonnaise",
-    imageUrl: "ItalianHerbMayonnaise.jpg",
+    imageUrl: ItaMayo,
   },
   {
     id: "mayo_007",
     title: "Barbeque Mayonnaise",
     type: "Mayonnaise",
     imageUrl: BarMayo,
-  },
-  {
-    id: "mayo_008",
-    title: "Chipotle Dip Mayonnaise",
-    type: "Mayonnaise",
-    imageUrl: "ChipotleDipMayonnaise.jpg",
   },
   {
     id: "mayo_009",
@@ -272,37 +155,13 @@ export const Portfolios = [
     id: "mayo_010",
     title: "Sandwich Spread",
     type: "Mayonnaise",
-    imageUrl: "SandwichSpread.jpg",
+    imageUrl: Sandwhich,
   },
   {
     id: "mayo_011",
     title: "Egg Mayonnaise",
     type: "Mayonnaise",
-    imageUrl: "EggMayonnaise.jpg",
-  },
-  {
-    id: "vinegar_001",
-    title: "Apple Cider Vinegar",
-    type: "Vinegars and Flavours",
-    imageUrl: "AppleCiderVinegar.jpg",
-  },
-  {
-    id: "vinegar_002",
-    title: "Synthetic Vinegar",
-    type: "Vinegars and Flavours",
-    imageUrl: "SyntheticVinegar.jpg",
-  },
-  {
-    id: "vinegar_003",
-    title: "White Vinegar",
-    type: "Vinegars and Flavours",
-    imageUrl: "WhiteVinegar.jpg",
-  },
-  {
-    id: "vinegar_004",
-    title: "Chinese Seasoning",
-    type: "Vinegars and Flavours",
-    imageUrl: "ChineseSeasoning.jpg",
+    imageUrl: Classic,
   },
 ];
 
