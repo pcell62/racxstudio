968015df6fdf89117a4c185f60a729eef85297c3
Arnav <arnav.verma2022@vitstudent.ac.in>
2024-11-16 17:27:48 +0530

    senbonzakura

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -4,14 +4,14 @@
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable import/extensions */
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { Fade } from 'react-awesome-reveal';
-import { Transition } from '@headlessui/react';
-import { useLocation } from 'react-router-dom';
+import { Fade } from "react-awesome-reveal";
+import { Transition } from "@headlessui/react";
+import { useLocation } from "react-router-dom";
 
-import Button from '../elements/Button';
-import BrandIcon from './BrandIcon';
+import Button from "../elements/Button";
+import BrandIcon from "./BrandIcon";
 
 export default function Header() {
   const [isCollapse, setIsCollapse] = useState(false);
@@ -23,10 +23,31 @@ export default function Header() {
       <div className="flex justify-between px-4 lg:px-0">
         <BrandIcon />
 
-        <button className="block text-theme-blue lg:hidden focus:outline-none" onClick={() => setIsCollapse(!isCollapse)}>
-          <svg className="w-8 h-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path className={`${isCollapse ? 'hidden' : 'block'}`} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
-            <path className={`${!isCollapse ? 'hidden' : 'block'}`} strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+        <button
+          className="block text-theme-blue lg:hidden focus:outline-none"
+          onClick={() => setIsCollapse(!isCollapse)}
+        >
+          <svg
+            className="w-8 h-8"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              className={`${isCollapse ? "hidden" : "block"}`}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16m-7 6h7"
+            />
+            <path
+              className={`${!isCollapse ? "hidden" : "block"}`}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M6 18L18 6M6 6l12 12"
+            />
           </svg>
         </button>
       </div>
@@ -34,7 +55,9 @@ export default function Header() {
       <ul className="hidden text-theme-blue tracking-widest items-center lg:flex flex-row mt-0">
         <li>
           <Button
-            className={`${path === '/' ? 'active-link' : ''} font-medium text-lg px-5 no-underline hover:underline`}
+            className={`${
+              path === "/" ? "active-link" : ""
+            } font-medium text-lg px-5 no-underline hover:underline`}
             type="link"
             href="/"
           >
@@ -43,7 +66,9 @@ export default function Header() {
         </li>
         <li className="py-2 lg:py-0">
           <Button
-            className={`${path === '/team' ? 'active-link' : ''} font-medium text-lg px-5 no-underline hover:underline`}
+            className={`${
+              path === "/team" ? "active-link" : ""
+            } font-medium text-lg px-5 no-underline hover:underline`}
             type="link"
             href="/team"
           >
@@ -52,7 +77,9 @@ export default function Header() {
         </li>
         <li className="py-2 lg:py-0">
           <Button
-            className={`${path === '/project' ? 'active-link' : ''} font-medium text-lg px-5 no-underline hover:underline`}
+            className={`${
+              path === "/project" ? "active-link" : ""
+            } font-medium text-lg px-5 no-underline hover:underline`}
             type="link"
             href="/project"
           >
@@ -61,7 +88,7 @@ export default function Header() {
         </li>
         <li>
           <Button
-            className="font-medium text-lg mx-auto ml-3 px-6 py-2 bg-theme-purple text-white rounded-full border-2 border-theme-purple hover:bg-dark-theme-purple border-purple-800 transition duration-200"
+            className="font-medium text-lg mx-auto ml-3 px-6 py-2 bg-theme-purple text-white rounded-full border-2 border-theme-purple hover:bg-theme-purple transition duration-200"
             type="link"
             href="/discuss-project"
           >
@@ -80,12 +107,13 @@ export default function Header() {
         leaveTo="opacity-0"
       >
         <div className="transition duration-300 ease-in data-[closed]:opacity-0">
-
           {/* <Fade> */}
           <ul className="z-50 flex flex-col text-theme-blue tracking-widest my-6 absolute bg-white w-full border-b-2 border-gray-300 lg:hidden">
             <li className="py-2 bg-white">
               <Button
-                className={`${path === '/' ? 'active-link' : ''} font-medium px-10 no-underline hover:underline`}
+                className={`${
+                  path === "/" ? "active-link" : ""
+                } font-medium px-10 no-underline hover:underline`}
                 type="link"
                 href="/"
               >
@@ -94,7 +122,9 @@ export default function Header() {
             </li>
             <li className="py-2 bg-white">
               <Button
-                className={`${path === '/team' ? 'active-link' : ''} font-medium px-10 no-underline hover:underline`}
+                className={`${
+                  path === "/team" ? "active-link" : ""
+                } font-medium px-10 no-underline hover:underline`}
                 type="link"
                 href="/team"
               >
@@ -103,7 +133,9 @@ export default function Header() {
             </li>
             <li className="py-2 bg-white">
               <Button
-                className={`${path === '/project' ? 'active-link' : ''} font-medium px-10 no-underline hover:underline`}
+                className={`${
+                  path === "/project" ? "active-link" : ""
+                } font-medium px-10 no-underline hover:underline`}
                 type="link"
                 href="/project"
               >
