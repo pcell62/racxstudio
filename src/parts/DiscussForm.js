386718ee60e3800734c83494de11ad3a5e7a8620
pcell62 <priyanshukumar10714@gmail.com>
2024-11-16 23:08:19 +0530

    23432

--- src/parts/DiscussForm.js
+++ src/parts/DiscussForm.js
@@ -20,15 +20,22 @@ export const DiscussForm = (actions) => {
   const { data, resetForm } = actions;
 
   const handleSubmit = () => {
-    const { name, company, email, phone } = data;
+    const { name, company, email, phone, projectIdea } = data;
 
-    if (name !== "" && company !== "" && email !== "" && phone !== "") {
+    if (
+      name !== "" &&
+      company !== "" &&
+      email !== "" &&
+      phone !== "" &&
+      projectIdea !== ""
+    ) {
       // Send email via EmailJS
       const templateParams = {
         name,
         company,
         email,
         phone,
+        projectIdea,
       };
 
       emailjs
@@ -117,6 +124,17 @@ export const DiscussForm = (actions) => {
             />
           </div>
 
+          <div className="mx-auto">
+            <Form
+              id="projectIdea"
+              name="projectIdea"
+              type="textarea"
+              value={data.projectIdea}
+              placeholder="Explain about your project idea"
+              className=""
+              onChange={actions.onChange}
+            />
+          </div>
           <Button
             className="text-xl mx-auto px-12 py-3 mt-5 bg-theme-purple text-white rounded-full border-2 border-theme-purple hover:bg-dark-theme-purple border-purple-800 transition duration-200 focus:outline-none"
             type="button"
