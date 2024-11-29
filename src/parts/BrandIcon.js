6af4ad5d7b75c06c71165aa694431a097678ffbb
pcell62 <priyanshukumar10714@gmail.com>
2024-11-29 22:30:54 +0530

    logo

--- src/parts/BrandIcon.js
+++ src/parts/BrandIcon.js
@@ -14,8 +14,8 @@ export default function BrandIcon() {
       <p className=" text-4xl text-[#d6b469] tracking-tighter font-medium flex justify-center items-center ">
         <img
           className="rounded-xl"
-          height={70}
-          width={70}
+          height={80}
+          width={80}
           src={BuildWebsite}
           alt="Build Website"
         />
